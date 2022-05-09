0e9538a7c772fb10ca1e21763b6552959784b4c3
chanthitlaos <102404129+chanthitlaos@users.noreply.github.com>
2022-05-09 22:59:48 +0700

    Add files via upload

---
+++ LoginPage/LoginPage/Form1.cs
@@ -0,0 +1,28 @@
+﻿namespace LoginPage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Username, Password;
+            Username = textBox1.Text;
+            Password = textBox2.Text;
+            if((Username == "souliya@gmail.com") && (Password == "123456"))
+            {
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.Show();
+                
+                
+            }
+            else { MessageBox.Show("ຂໍ້ມູນບໍ່ຖືກຕ້ອງ");
+
+            }
+        }
+    }
+}
